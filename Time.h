a71f2cd583f7089be676842565359806aa1a1995
Yeonsoo So <65909443+yeonsooSo@users.noreply.github.com>
2020-09-09 14:22:08 +0900

    Update Time.h

--- Time.h
+++ Time.h
@@ -1,12 +1,22 @@
 #pragma once
 #include <string>
 
-#ifndef TIME_H
-#define TIME_H
+#ifndef TIME_H //정의되어있지 않으면
+#define TIME_H //지금 정의
 
 class Time {
 public:
+	explicit Time(int = 0, int = 0, int = 0);
+
 	void setTime(int, int, int);
+	void setHour(int);
+	void setMinute(int);
+	void setSecond(int);
+
+	unsigned int getHour() const;
+	unsigned int getMinute() const;
+	unsigned int getSecond() const;
+
 	std::string toUniversalString() const;
 	std::string toStandardString() const;
 
@@ -15,4 +25,4 @@ private:
 	unsigned int minute{ 0 };
 	unsigned int second{ 0 };
 };
-#endif
+#endif //여기까지 실행 (정의되어있지 않다면. 만약 정의되어 있다면 여기까지 스킵)
