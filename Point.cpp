5e32ec7ea5ba76fce401a142beac264c5e29d448
Yeonsoo So <65909443+yeonsooSo@users.noreply.github.com>
2020-09-14 01:46:47 +0900

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -2,10 +2,14 @@
 #include <sstream>
 #include <iomanip>
 using namespace std;
-  
+
 string Point::toString() {
 	ostringstream out;
 	out << "x = " << setfill('0') << setw(2)<< x << ", " 
-		<< setw(2) << "y = " << y << endl;
+		<< "y = " << setw(2) << y << endl; // 스트림에 저장
 	return out.str();
+}
+void Point::setPoint(int xx, int yy) {
+	x = xx;
+	y = yy;
 }
