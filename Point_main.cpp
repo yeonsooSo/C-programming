03a02b6720e55ff1e22c3036b5e6c105c19c4abd
Yeonsoo So <65909443+yeonsooSo@users.noreply.github.com>
2020-09-14 01:47:31 +0900

    Update Point_main.cpp

--- Point_main.cpp
+++ Point_main.cpp
@@ -1,16 +1,35 @@
 #include "Point.h"
-//try catch! 잘못된 범위 들어오면 메시지 출력하고 좌표 다시 받기
-
+#include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
 int main() {
-	Point p1;
-	Point p2;
-	Point p3;
-	Point p4;
-	Point p5;
 
+	int n{ 5 }; // 좌표 개수
+	int x, y;
 
-	Point p[5];
+	Point p[5]; // Point형 배열 선언
+
+	for (int i{ 0 }; i < n; i++) {
+		cout << "Input x and y for Point: ";
+		try {
+			cin >> x >> y;
+			if ((x > 99 || x <= 0) || (y > 99 || y <= 0)) { // 입력된 좌표의 범위에 대한 예외처리
+				throw invalid_argument("input again.\n");
+			}
+			else{ p[i].setPoint(x, y); }
+		}
+		catch (invalid_argument& e) {
+			cerr << "out of range: " << e.what() << endl;
+			i--; // 예외 있을 경우 좌표 다시 받기
+		}
+	}
+	//display
+	cout << endl;
+	for (int i{ 0 }; i < n; i++) {
+		Point pRef = p[i];
+		cout << pRef.toString(); // 각 개체의 레퍼런스로 함수호출
+	}
+	return 0;
 }
