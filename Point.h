8617a2005fd97d59f45eea4162adc74f0dc28ad9
Yeonsoo So <65909443+yeonsooSo@users.noreply.github.com>
2020-09-14 01:47:08 +0900

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,14 +1,17 @@
-#pragma once
 #include <string>
-//기본생성자
+
+#ifndef POINT_H
+#define POINT_H
 
 class Point {
 public:
 	Point() {}
 	~Point() {}
 	std::string toString();
+	void setPoint(int, int);
 
 private:
 	int x;
 	int y;
 };
+#endif
